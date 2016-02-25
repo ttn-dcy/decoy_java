ddc8f14664745d93cf64b1ed178f38a59a15cd4b
ttn-dcy <h1976204@trbvm.com>
2016-02-25 12:00:00 +0100

    ghdecoy

--- decoy.java
+++ decoy.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World34");
+        System.out.println("Hello, World35");
     }
 
 }
